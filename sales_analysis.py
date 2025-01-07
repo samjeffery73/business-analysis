2290b1ea5122ec1ba314db94854ac9cd525bbf7d
Sam Jeffery <samjeffery73@yahoo.com>
2025-01-07 16:47:46 -0500

    Added description to readme, fixed initial comments

--- sales_analysis.py
+++ sales_analysis.py
@@ -3,29 +3,9 @@ Date: 01/07/2025
 
 Developed by: Sam Jeffery
 
-Objective:
+For more information on this project please refer to the README.md file on the GitHub repository.
 
-Demonstrate my ability to create an end to end Data Analysis project by analyzing sales data, identifying trends, KPIs.
-Then, visualize and input the data into a dashboard.
-
-This program is NOT object oriented.
-
-1. Start by collecting data. For my own purposes, I will be pulling data from an API that contains sales data of some sort. 
-I will add the resource here once I figure out what I am using.
-
-2. Explore the data. Perform exploratory analysis, summary statistics, identify trends. Take note of any fields that are missing or invalid.
-
-3. Clean the data. Handle missing data, remove outliers by leveraging Z Scores or IQR, standardize all dates, currencies, and categorical variables.
-Assure all columns are descriptive and readable.
-
-4. Transform the data. Add calculated collumns and/or aggregations. Group data by categories. Use pivot tables or groupby operations.
-
-5. Analyze Trends. Look for patterns, correlations, seasonality. Compare groups or categories to find differences. Use simple visualizations.
-
-6. Build a Dashboard. Using Tableau, BI, or Python-based dashboards like Dash or Streamlit. Include an overview, detailed sections, and interactability.
-
-7. Identify Key Insights. From the visualizations, identify and address unexpected trends, anomolies, and other key findings.
-
-8. Finally, Document and reflect on the process.
+To view my report and dashboard, please view the pictures and document that are also in the repository.
 
 '''
+
